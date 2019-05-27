0677aa714567b33f14d4a364a7b4614a5082c637
Edgar <zorro.ej@gmail.com>
2019-05-28 05:01:43 +0800

    add curried form

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import formWrapperCreater from './src/formWrapper'
 
+const curried = wrapper => (
+  validator => (
+    store => (
+      Form => (
+        wrapper({Form, store, validator})
+      )
+    )
+  )
+)
+
+export const curriedFormWrapperCreater = curried(formWrapperCreater)
+
 export default formWrapperCreater
